649cb731015c51987ad03065d0c76bffb71cfb6b
aleegs <aleej.gs@gmail.com>
2021-04-07 01:37:44 -0300

    Move treact's three col features component

--- frontend/src/components/ThreeColFeatures.jsx
+++ frontend/src/components/ThreeColFeatures.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-//eslint-disable-next-line
-import { css } from "styled-components/macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
 import {
   SectionHeading,
   Subheading as SubheadingBase,
-} from "treact/components/misc/Headings.js";
-import { SectionDescription } from "treact/components/misc/Typography.js";
-
-import defaultCardImage from "treact/images/shield-icon.svg";
+} from "treact/components/misc/Headings";
+import { SectionDescription } from "treact/components/misc/Typography";
 
 import { ReactComponent as SvgDecoratorBlob3 } from "treact/images/svg-decorator-blob-3.svg";
 
@@ -61,21 +58,15 @@ const DecoratorBlob = styled(SvgDecoratorBlob3)`
   ${tw`pointer-events-none absolute right-0 bottom-0 w-64 opacity-25 transform translate-x-32 translate-y-48 `}
 `;
 
-export default ({
-  cards = null,
-  heading = "Amazing Features",
-  subheading = "Features",
-  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-}) => {
-  /*
-   * This componets has an array of object denoting the cards defined below. Each object in the cards array can have the key (Change it according to your need, you can also add more objects to have more cards in this feature component) or you can directly pass this using the cards prop:
-   *  1) imageSrc - the image shown at the top of the card
-   *  2) title - the title of the card
-   *  3) description - the description of the card
-   *  If a key for a particular card is not provided, a default value is used
-   */
-
-  const defaultCards = [
+/*
+ * This componets has an array of object denoting the cards defined below. Each object in the cards array can have the key (Change it according to your need, you can also add more objects to have more cards in this feature component) or you can directly pass this using the cards prop:
+ *  1) imageSrc - the image shown at the top of the card
+ *  2) title - the title of the card
+ *  3) description - the description of the card
+ *  If a key for a particular card is not provided, a default value is used
+ */
+const properties = {
+  cards: [
     {
       imageSrc: ShieldIconImage,
       title: "Secure",
@@ -87,25 +78,33 @@ export default ({
     { imageSrc: ReliableIconImage, title: "Reliable" },
     { imageSrc: FastIconImage, title: "Fast" },
     { imageSrc: SimpleIconImage, title: "Easy" },
-  ];
+  ],
+  heading: "Amazing Features",
+  subheading: "Features",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+};
 
-  if (!cards) cards = defaultCards;
-
+export default () => {
   return (
     <Container>
       <ThreeColumnContainer>
-        {subheading && <Subheading>{subheading}</Subheading>}
-        <Heading>{heading}</Heading>
-        {description && <Description>{description}</Description>}
+        {properties.subheading && (
+          <Subheading>{properties.subheading}</Subheading>
+        )}
+        <Heading>{properties.heading}</Heading>
+        {properties.description && (
+          <Description>{properties.description}</Description>
+        )}
         <VerticalSpacer />
-        {cards.map((card, i) => (
-          <Column key={i}>
+        {properties.cards.map((card) => (
+          <Column key={card.title}>
             <Card>
               <span className="imageContainer">
-                <img src={card.imageSrc || defaultCardImage} alt="" />
+                <img src={card.imageSrc || ShieldIconImage} alt="" />
               </span>
               <span className="textContainer">
-                <span className="title">{card.title || "Fully Secure"}</span>
+                <span className="title">{card.title}</span>
                 <p className="description">
                   {card.description ||
                     "Lorem ipsum donor amet siti ceali ut enim ad minim veniam, quis nostrud."}
