88af703d25106ec461b066e808b31707f18e080d
aleegs <aleej.gs@gmail.com>
2021-04-07 01:34:59 -0300

    Move treact's two col features component

--- frontend/src/components/TwoColFeatures.jsx
+++ frontend/src/components/TwoColFeatures.jsx
@@ -5,7 +5,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 import {
   SectionHeading,
   Subheading as SubheadingBase,
-} from "treact/components/misc/Headings.js";
+} from "treact/components/misc/Headings";
 import { PrimaryButton as PrimaryButtonBase } from "treact/components/misc/Buttons";
 import StatsIllustrationSrc from "treact/images/stats-illustration.svg";
 import { ReactComponent as SvgDotPattern } from "treact/images/dot-pattern.svg";
@@ -42,33 +42,29 @@ const PrimaryButton = tw(
   PrimaryButtonBase
 )`mt-8 md:mt-10 text-sm inline-block mx-auto md:mx-0`;
 
-const DecoratorBlob = styled(SvgDotPattern)((props) => [
+const DecoratorBlob = styled(SvgDotPattern)(() => [
   tw`w-20 h-20 absolute right-0 bottom-0 transform translate-x-1/2 translate-y-1/2 fill-current text-primary-500 -z-10`,
 ]);
 
-export default ({
-  subheading = "How it works",
-  heading = (
+const properties = {
+  subheading: "How it works",
+  heading: (
     <>
       Effective financial management in <wbr />
       <span tw="text-primary-500">one app</span>
     </>
   ),
-  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  primaryButtonText = "Learn More",
-  primaryButtonUrl = "https://timerse.com",
-  imageSrc = StatsIllustrationSrc,
-  imageCss = null,
-  imageContainerCss = null,
-  imageDecoratorBlob = false,
-  imageDecoratorBlobCss = null,
-  imageInsideDiv = true,
-  statistics = null,
-  textOnLeft = false,
-}) => {
-  // The textOnLeft boolean prop can be used to display either the text on left or right side of the image.
-  //Change the statistics variable as you like, add or delete objects
-  const defaultStatistics = [
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  primaryButtonText: "Learn More",
+  primaryButtonUrl: "https://timerse.com",
+  imageSrc: StatsIllustrationSrc,
+  imageCss: null,
+  imageContainerCss: null,
+  imageDecoratorBlob: false,
+  imageDecoratorBlobCss: null,
+  imageInsideDiv: true,
+  statistics: [
     {
       key: "Clients",
       value: "2282+",
@@ -81,36 +77,43 @@ export default ({
       key: "Awards",
       value: "1000+",
     },
-  ];
+  ],
+  textOnLeft: false,
+};
 
-  if (!statistics) statistics = defaultStatistics;
+export default () => {
+  // The textOnLeft boolean prop can be used to display either the text on left or right side of the image.
 
   return (
     <Container>
-      <TwoColumn css={!imageInsideDiv && tw`md:items-center`}>
-        <ImageColumn css={imageContainerCss}>
-          {imageInsideDiv ? (
-            <Image imageSrc={imageSrc} css={imageCss} />
+      <TwoColumn css={!properties.imageInsideDiv && tw`md:items-center`}>
+        <ImageColumn css={properties.imageContainerCss}>
+          {properties.imageInsideDiv ? (
+            <Image imageSrc={properties.imageSrc} css={properties.imageCss} />
           ) : (
-            <img src={imageSrc} css={imageCss} alt="" />
+            <img src={properties.imageSrc} css={properties.imageCss} alt="" />
           )}
-          {imageDecoratorBlob && <DecoratorBlob css={imageDecoratorBlobCss} />}
+          {properties.imageDecoratorBlob && (
+            <DecoratorBlob css={properties.imageDecoratorBlobCss} />
+          )}
         </ImageColumn>
-        <TextColumn textOnLeft={textOnLeft}>
+        <TextColumn textOnLeft={properties.textOnLeft}>
           <TextContent>
-            {subheading && <Subheading>{subheading}</Subheading>}
-            <Heading>{heading}</Heading>
-            <Description>{description}</Description>
+            {properties.subheading && (
+              <Subheading>{properties.subheading}</Subheading>
+            )}
+            <Heading>{properties.heading}</Heading>
+            <Description>{properties.description}</Description>
             <Statistics>
-              {statistics.map((statistic, index) => (
-                <Statistic key={index}>
+              {properties.statistics.map((statistic) => (
+                <Statistic key={statistic.key}>
                   <Value>{statistic.value}</Value>
                   <Key>{statistic.key}</Key>
                 </Statistic>
               ))}
             </Statistics>
-            <PrimaryButton as="a" href={primaryButtonUrl}>
-              {primaryButtonText}
+            <PrimaryButton as="a" href={properties.primaryButtonUrl}>
+              {properties.primaryButtonText}
             </PrimaryButton>
           </TextContent>
         </TextColumn>
