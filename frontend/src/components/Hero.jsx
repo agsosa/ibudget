f78ed147dc5bcae62be19a922aca33bf81fd2c0a
aleegs <aleej.gs@gmail.com>
2021-04-07 02:58:56 -0300

    Update Hero to use react-router Link

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
+import { Link } from "react-router-dom";
 
 import ReactModalAdapter from "treact/helpers/ReactModalAdapter";
 import ResponsiveVideoEmbed from "treact/helpers/ResponsiveVideoEmbed";
@@ -23,7 +24,11 @@ const Heading = tw.h1`font-black text-3xl md:text-5xl leading-snug max-w-3xl`;
 const Paragraph = tw.p`my-5 lg:my-8 text-sm lg:text-base font-medium text-gray-600 max-w-lg mx-auto lg:mx-0`;
 
 const Actions = tw.div`flex flex-col items-center sm:flex-row justify-center lg:justify-start mt-8`;
-const PrimaryButton = tw.button`font-bold px-8 lg:px-10 py-3 rounded bg-primary-500 text-gray-100 hocus:bg-primary-600 focus:shadow-outline focus:outline-none transition duration-300`;
+
+const PrimaryButton = tw(
+  Link
+)`font-bold px-8 lg:px-10 py-3 rounded bg-primary-500 text-gray-100 hocus:bg-primary-600 focus:shadow-outline hocus:text-gray-100 focus:outline-none transition duration-300`;
+
 const WatchVideoButton = styled.button`
   ${tw`mt-4 sm:mt-0 sm:ml-8 flex items-center text-secondary-300 transition duration-300 hocus:text-primary-400 focus:outline-none`}
   .playIcon {
@@ -55,6 +60,7 @@ const StyledModal = styled(ReactModalAdapter)`
     ${tw`w-full lg:p-16`}
   }
 `;
+
 const CloseModalButton = tw.button`absolute top-0 right-0 mt-8 mr-8 hocus:text-primary-500`;
 
 /* End styled components */
@@ -85,7 +91,7 @@ export default () => {
             <Heading>{properties.heading}</Heading>
             <Paragraph>{properties.description}</Paragraph>
             <Actions>
-              <PrimaryButton as="a" href={properties.primaryButtonUrl}>
+              <PrimaryButton to={properties.primaryButtonUrl}>
                 {properties.primaryButtonText}
               </PrimaryButton>
               <WatchVideoButton onClick={toggleModal}>
