dc4ffb51673238b93d0c9241578173ac26c60cb0
aleegs <aleej.gs@gmail.com>
2021-04-07 02:15:45 -0300

    Update Footer links

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,8 +3,10 @@ import tw from "twin.macro";
 import { Container as ContainerBase } from "treact/components/misc/Layouts";
 import logo from "treact/images/logo.svg";
 import { APP_NAME } from "lib/Config";
+import { Link } from "react-router-dom";
 
 /* Start styled components */
+
 const Container = tw(ContainerBase)`bg-gray-900 text-gray-100 -mx-8 -mb-8`;
 const Content = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
 
@@ -15,9 +17,12 @@ const LogoImg = tw.img`w-8`;
 const LogoText = tw.h5`ml-2 text-2xl font-black tracking-wider`;
 
 const LinksContainer = tw.div`mt-8 font-medium flex flex-wrap justify-center items-center flex-col sm:flex-row`;
-const Link = tw.a`text-white border-b-2 border-transparent hocus:text-primary-400 hocus:border-primary-500 pb-1 transition duration-300 mt-2 mx-4`;
+const LinkStyle = tw(
+  Link
+)`text-white border-b-2 border-transparent hocus:text-primary-400 hocus:border-primary-500 pb-1 transition duration-300 mt-2 mx-4`;
 
 const CopyrightText = tw.p`text-center mt-10 font-medium tracking-wide text-sm text-gray-600`;
+
 /* End styled components */
 
 export default () => {
@@ -31,11 +36,15 @@ export default () => {
           </LogoContainer>
 
           <LinksContainer>
-            <Link href="/">Home</Link>
-            <Link href="/">Contact Us</Link>
-            <Link href="/">How to Use</Link>
-            <Link href="/">Privacy Policy</Link>
-            <Link href="/">Terms of Service</Link>
+            <LinkStyle to="/">Home</LinkStyle>
+
+            <LinkStyle to="/contact-us">Contact Us</LinkStyle>
+
+            <LinkStyle to="/how-it-works">How it Works</LinkStyle>
+
+            <LinkStyle to="/privacy-policy">Privacy Policy</LinkStyle>
+
+            <LinkStyle to="/terms-of-service">Terms of Service</LinkStyle>
           </LinksContainer>
 
           <CopyrightText>
