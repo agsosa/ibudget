c76f5df741bb43a3cc8e51a818e4583832b264c9
aleegs <aleej.gs@gmail.com>
2021-04-07 03:49:02 -0300

    Refactor AnimationRevealPage

--- frontend/src/components/AnimationRevealPage.jsx
+++ frontend/src/components/AnimationRevealPage.jsx
@@ -1,57 +1,47 @@
-import React from "react";
-import tw from "twin.macro";
+import * as React from "react";
 
-/* framer-motion and useInView here are used to animate the sections in when we reach them in the viewport */
-import { motion } from "framer-motion";
-import useInView from "use-in-view";
+import { motion, AnimatePresence } from "framer-motion";
+import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 
-const StyledDiv = tw.div`font-display min-h-screen text-secondary-500 p-8 overflow-hidden`;
+function AnimationRevealPage({ disabled, children }) {
+  let childrenElements = children;
 
-function AnimatedSlideInComponent({ children }) {
-  const [ref, inView] = useInView(30);
+  // React-Router animate on patah change
+  const { pathname } = useLocation();
 
-  const x = { target: "0%" };
+  // Process children object
+  if (!disabled) {
+    if (!Array.isArray(children)) childrenElements = [children];
 
-  if (direction === "left") x.initial = "-150%";
-  else x.initial = "150%";
-
-  return (
-    <motion.section
-      initial={{ x: x.initial }}
-      exit={{ opacity: 0 }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ type: "just", duration: 1 }}
-      ref={ref}
-    >
-      {children}
-    </motion.section>
-  );
-}
-
-function AnimationReveal({ disabled, children }) {
-  if (disabled) {
-    return <>{children}</>;
+    childrenElements = childrenElements.map((child, i) => {
+      return (
+        <AnimatePresence exitBeforeEnter key={pathname}>
+          <motion.section
+            key={i} // eslint-disable-line
+            exit={{ opacity: 0 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ type: "tween", duration: 0.8 }}
+          >
+            {child}
+          </motion.section>
+        </AnimatePresence>
+      );
+    });
   }
 
-  if (!Array.isArray(children)) children = [children];
-
-  const directions = ["left", "right"];
-  const childrenWithAnimation = children.map((child, i) => {
-    return (
-      <AnimatedSlideInComponent
-        key={i}
-        direction={directions[i % directions.length]}
-      >
-        {child}
-      </AnimatedSlideInComponent>
-    );
-  });
-  return <>{childrenWithAnimation}</>;
+  return <>{childrenElements}</>;
 }
 
-export default (props) => (
-  <StyledDiv className="App">
-    <AnimationReveal {...props} />
-  </StyledDiv>
-);
+AnimationRevealPage.defaultProps = {
+  disabled: false,
+  children: [],
+};
+
+AnimationRevealPage.propTypes = {
+  disabled: PropTypes.bool,
+  children: PropTypes.node,
+};
+
+export default AnimationRevealPage;
--- frontend/src/pages/Routes.jsx
+++ frontend/src/pages/Routes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import tw from "twin.macro";
 
 import HomePage from "pages/HomePage";
 import DashboardPage from "pages/DashboardPage";
@@ -8,42 +9,49 @@ import HowItWorksPage from "pages/HowItWorksPage";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 import ScrollToTop from "components/ScrollToTop";
+import AnimationRevealPage from "components/AnimationRevealPage";
 import ContactUsPage from "./ContactUsPage";
 import PrivacyPolicyPage from "./PrivacyPolicyPage";
 import TermsOfServicePage from "./TermsOfServicePage";
 
+const StyledDiv = tw.div`font-display min-h-screen text-secondary-500 p-8 overflow-hidden`;
+
 function Routes() {
   return (
     <Router>
-      <ScrollToTop />
-      <Navbar />
-      <Switch>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/register">
-          <RegisterPage />
-        </Route>
-        <Route path="/how-it-works">
-          <HowItWorksPage />
-        </Route>
-        <Route path="/contact-us">
-          <ContactUsPage />
-        </Route>
-        <Route path="/terms-of-service">
-          <TermsOfServicePage />
-        </Route>
-        <Route path="/privacy-policy">
-          <PrivacyPolicyPage />
-        </Route>
-        <Route path="/dashboard">
-          <DashboardPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-      <Footer />
+      <StyledDiv className="App">
+        <ScrollToTop />
+        <Navbar />
+        <AnimationRevealPage>
+          <Switch>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/register">
+              <RegisterPage />
+            </Route>
+            <Route path="/how-it-works">
+              <HowItWorksPage />
+            </Route>
+            <Route path="/contact-us">
+              <ContactUsPage />
+            </Route>
+            <Route path="/terms-of-service">
+              <TermsOfServicePage />
+            </Route>
+            <Route path="/privacy-policy">
+              <PrivacyPolicyPage />
+            </Route>
+            <Route path="/dashboard">
+              <DashboardPage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </AnimationRevealPage>
+        <Footer />
+      </StyledDiv>
     </Router>
   );
 }
