9633680028f39a3736aa83961b6665dcea656610
aleegs <aleej.gs@gmail.com>
2021-04-07 23:48:23 -0300

    Create AreaChart

---
+++ frontend/src/components/dashboard/AreaChart.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+import * as React from "react";
+import { Area } from "@ant-design/charts";
+
+function Chart() {
+  const [data, setData] = React.useState([]);
+
+  const asyncFetch = () => {
+    fetch(
+      "https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json"
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.log("fetch data failed", error);
+      });
+  };
+
+  React.useEffect(() => {
+    asyncFetch();
+  }, []);
+
+  const config = {
+    data,
+    xField: "Date",
+    yField: "scales",
+    xAxis: { tickCount: 5 },
+    slider: {
+      start: 0,
+      end: 1,
+      trendCfg: { isArea: true },
+    },
+  };
+
+  return <Area {...config} />;
+}
+
+export default React.memo(Chart);
