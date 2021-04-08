ad2aaf1f5e8b8dd7125bb8512f25dbc55be13d17
aleegs <aleej.gs@gmail.com>
2021-04-08 19:24:02 -0300

    Implement getCategoryColor

--- frontend/src/lib/Helpers.js
+++ frontend/src/lib/Helpers.js
@@ -1,3 +1,6 @@
+import tailwind from "tailwind.config";
+import { EnumCategory } from "./Enums";
+
 // Javascript number formatter
 export const moneyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
@@ -7,3 +10,15 @@ export const moneyFormatter = new Intl.NumberFormat("en-US", {
   minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
   //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
+
+// TODO: Add more categories colors
+export function getCategoryColor(enumCategory) {
+  switch (enumCategory) {
+    case EnumCategory.SHOPPING:
+      return tailwind.theme.colors.orange["500"];
+    case EnumCategory.COMMUNICATION_PC:
+      return tailwind.theme.colors.blue["500"];
+    default:
+      return "white";
+  }
+}
