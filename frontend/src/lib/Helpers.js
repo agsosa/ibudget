e2786b99920c15c6f7954e56dec8582bc5bb8458
aleegs <aleej.gs@gmail.com>
2021-04-08 19:51:02 -0300

    remove getCategoryColor

--- frontend/src/lib/Helpers.js
+++ frontend/src/lib/Helpers.js
@@ -1,5 +1,3 @@
-import { EnumCategory } from "./Enums";
-
 // Javascript NumberFormat object to format money
 const moneyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
@@ -14,21 +12,4 @@ const moneyFormatter = new Intl.NumberFormat("en-US", {
  */
 export function getMoneyDisplayString(money) {
   return moneyFormatter.format(money);
-}
-
-/**
- * Get the associated tailwind color for a category
- * @param  {String} enumCategory The category (must be a value of EnumCategory)
- * @return {String}              Tailwind color
- */
-export function getCategoryColor(enumCategory) {
-  // TODO: Add more categories colors
-  switch (enumCategory) {
-    case EnumCategory.SHOPPING:
-      return "orange-500";
-    case EnumCategory.COMMUNICATION_PC:
-      return "blue-500";
-    default:
-      return "white";
-  }
 }
