4cccdce54a47b84f6a0be60e4d3859ac6f7dd67b
aleegs <aleej.gs@gmail.com>
2021-04-07 22:11:50 -0300

    Add ant-design charts

---
+++ frontend/src/components/dashboard/Chart.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+import * as React from "react";
+import { Line } from "@ant-design/charts";
+
+const data = [
+  { year: "1991", value1: 3 },
+  { year: "1992", value: 4 },
+  { year: "1993", value: 3.5 },
+  { year: "1994", value: 5 },
+  { year: "1995", value: 4.9 },
+  { year: "1996", value: 6 },
+  { year: "1997", value: 7 },
+  { year: "1998", value: 9 },
+  { year: "1999", value: 13 },
+];
+
+const config = {
+  data,
+  height: 400,
+  xField: "year",
+  yField: "value",
+  point: {
+    size: 5,
+    shape: "diamond",
+  },
+  label: {
+    style: {
+      fill: "#aaa",
+    },
+  },
+};
+
+function Chart() {
+  return <Line {...config} />;
+}
+
+export default React.memo(Chart);
