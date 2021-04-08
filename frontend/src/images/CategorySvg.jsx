fadd3d9ba6c3cc6a5f259b4c35dc6a059e71434b
aleegs <aleej.gs@gmail.com>
2021-04-08 19:12:00 -0300

    Rename CategoryIcons

---
+++ frontend/src/images/CategorySvg.jsx
@@ -0,0 +1,47 @@
+export const SHOPPING = (
+  <svg
+    width="40"
+    height="40"
+    style={{ display: "block", margin: "auto", marginTop: 2 }}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="white"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+    />
+  </svg>
+);
+
+export const FOOD = (
+  <svg
+    width="40"
+    height="40"
+    style={{ display: "block", margin: "auto", marginTop: 2 }}
+    fill="none"
+    viewBox="0 0 64 64"
+    stroke="white"
+  >
+    <path
+      data-name="layer2"
+      d="M52.5 15.5c0-5.5-3.6-13.7-8-13.7s-8 8.1-8 13.7 2.5 8.1 6 9l-1 33.5c0 3.2 1.9 4 3 4s3.2-.6 3-4l-1-33.5c3.4-.9 6-4.3 6-9z"
+      fill="none"
+      stroke="white"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    />
+    <path
+      data-name="layer1"
+      d="M24.5 2.1h-12l-1 20c0 3.2 2.1 5.2 5 5.8v32.2a2 2 0 0 0 4 0V27.9c2.9-.6 5-2.6 5-5.8z"
+      fill="none"
+      stroke="white"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    />
+  </svg>
+);
