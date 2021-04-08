dac4015a797cfe7a8d647f19b328bf871daaa17d
aleegs <aleej.gs@gmail.com>
2021-04-07 22:27:14 -0300

    Refactor Card

--- frontend/src/components/dashboard/Card.jsx
+++ frontend/src/components/dashboard/Card.jsx
@@ -1,90 +1,54 @@
-/* eslint-disable  */
-
 import React from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
-import { PrimaryButton as PrimaryButtonBase } from "treact/components/misc/Buttons";
-import { ReactComponent as CheckboxIcon } from "treact/images/checkbox-circle.svg";
+import { PropTypes } from "prop-types";
 
-const Card = styled.div`
-  ${tw`w-full max-w-sm bg-white rounded-lg shadow-sm py-10 px-6 sm:px-10 lg:px-6 lg:py-10 xl:p-10 mx-3 flex flex-col justify-between mt-16 first:mt-0 lg:mt-0 shadow-raised`}
+const CardBlock = styled.div`
+  ${tw`w-full max-w-sm bg-white rounded-lg shadow-sm py-10 px-6 sm:px-10 lg:px-6 lg:py-10 xl:p-8 mx-3 flex flex-col justify-between mt-16 first:mt-0 lg:mt-0 shadow-raised`}
 `;
 
 const CardHeader = styled.div`
-  .nameAndFeaturedContainer {
-    ${tw`flex flex-wrap flex-col sm:flex-row justify-between items-center`}
+  .headerContainer {
+    ${tw`flex flex-wrap flex-col sm:flex-row justify-between items-center border-b pb-3`}
   }
   .name {
     ${tw`lg:text-lg xl:text-xl font-bold uppercase tracking-wider mr-3`}
   }
-  .featuredText {
-    ${tw`text-xs font-bold px-3 rounded py-2 uppercase bg-green-300 text-green-900 leading-none mt-4 sm:mt-0 w-full sm:w-auto text-center`}
-  }
-  .pricingContainer {
-    ${tw`mt-6 flex items-end justify-between`}
-    .currentPrice {
-      ${tw`text-lg font-bold leading-none`}
-      .bigText {
-        ${tw`text-3xl font-bold`}
-      }
-    }
-    .oldPrice {
-      ${tw`text-gray-500 text-lg line-through hidden sm:block`}
-    }
-  }
-  .description {
+  .childrenContainer {
     ${tw`mt-8 font-medium text-gray-700 lg:text-sm xl:text-base`}
   }
 `;
-const CardFeatures = styled.ul`
-  ${tw`mt-10 flex-1 border-t lg:-mx-6 -mx-6 sm:-mx-10 py-10 px-6 sm:px-10 lg:p-6 xl:-mx-10 xl:p-10`}
-  .feature {
-    ${tw`flex items-start mt-6 first:mt-0`}
-    .icon {
-      ${tw`w-6 h-6 text-primary-500 flex-shrink-0`}
-    }
-    .text {
-      ${tw`font-semibold text-primary-900 tracking-wide ml-3`}
-    }
-  }
-`;
 
-const CardAction = tw.div`mt-4`;
-const ActionButton = styled(PrimaryButtonBase)`
-  ${tw`block text-center text-sm font-semibold tracking-wider w-full text-gray-100 bg-primary-500 px-6 py-4 rounded hover:bg-primary-700 focus:shadow-outline focus:outline-none transition-colors duration-300`}
-`;
-
-export default () => {
+function Card({ title, RightHeaderComponent, children }) {
   return (
-    <Card featured>
+    <CardBlock featured>
       <CardHeader>
-        <span className="nameAndFeaturedContainer">
-          <span className="name">name</span>
+        <span className="headerContainer">
+          <span className="name">{title}</span>
 
-          <span className="featuredText">feat</span>
+          {RightHeaderComponent && (
+            <span>
+              <RightHeaderComponent />
+            </span>
+          )}
         </span>
-        <div className="pricingContainer">
-          <span className="currentPrice">
-            <span className="bigText">25</span>
-            55
-          </span>
-
-          <span className="oldPrice">23</span>
-        </div>
-        <p className="description">desc</p>
+        {children && <div className="childrenContainer">{children}</div>}
       </CardHeader>
-      <CardFeatures>
-        <li className="feature">
-          <CheckboxIcon className="icon" />
-          <span className="text">feat</span>
-        </li>
-      </CardFeatures>
-      <CardAction>
-        <ActionButton as="a" href="/">
-          text
-        </ActionButton>
-      </CardAction>
-    </Card>
+    </CardBlock>
   );
+}
+
+Card.defaultProps = {
+  title: "",
+  RightHeaderComponent: null,
+  children: null,
 };
+
+Card.propTypes = {
+  title: PropTypes.string,
+  RightHeaderComponent: PropTypes.element,
+  children: PropTypes.node,
+};
+
+export default Card;
