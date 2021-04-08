14d5f578e143e8955f22df2672df2044d5f0e3cd
aleegs <aleej.gs@gmail.com>
2021-04-08 19:34:26 -0300

    Add function jsdoc

--- frontend/src/lib/Helpers.js
+++ frontend/src/lib/Helpers.js
@@ -1,23 +1,33 @@
-import tailwind from "tailwind.config";
 import { EnumCategory } from "./Enums";
 
-// Javascript number formatter
-export const moneyFormatter = new Intl.NumberFormat("en-US", {
+// Javascript NumberFormat object to format money
+const moneyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
-
-  // These options are needed to round to whole numbers if that's what you want.
-  minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  minimumFractionDigits: 0,
 });
 
-// TODO: Add more categories colors
+/**
+ * Format a number as a currency
+ * @param  {Number} money The number (money amount)
+ * @return {String}       Formatted money number
+ */
+export function getMoneyDisplayString(money) {
+  return moneyFormatter.format(money);
+}
+
+/**
+ * Get the associated tailwind color for a category
+ * @param  {String} enumCategory The category (must be a value of EnumCategory)
+ * @return {String}              Tailwind color
+ */
 export function getCategoryColor(enumCategory) {
+  // TODO: Add more categories colors
   switch (enumCategory) {
     case EnumCategory.SHOPPING:
-      return tailwind.theme.colors.orange["500"];
+      return "orange-500";
     case EnumCategory.COMMUNICATION_PC:
-      return tailwind.theme.colors.blue["500"];
+      return "blue-500";
     default:
       return "white";
   }
