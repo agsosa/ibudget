ff5486f172c7e7c06f994795870ff667bb4e54b8
aleegs <aleej.gs@gmail.com>
2021-04-08 03:56:07 -0300

    Update tailwind config

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -313,7 +313,7 @@ module.exports = {
       "2xl": "1.5rem",
       "3xl": "1.875rem",
       "4xl": "2.25rem",
-      "5xl": "3rem",
+      "5xl": "3.1rem",
       "6xl": "4rem",
     },
     fontWeight: {
