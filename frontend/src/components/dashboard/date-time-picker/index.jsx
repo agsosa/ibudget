265fa9bc3506d22ebfc0aee175c0b5ef1446c932
aleegs <aleej.gs@gmail.com>
2021-04-08 03:55:55 -0300

    Add Label

--- frontend/src/components/dashboard/date-time-picker/index.jsx
+++ frontend/src/components/dashboard/date-time-picker/index.jsx
@@ -1,5 +1,14 @@
+import tw from "twin.macro";
 import DateTimeDropdown from "./DateTimeDropdown";
 
+const Container = tw.div`mt-8 flex flex-row justify-center`;
+const Label = tw.text`text-gray-600 mr-3 top-0 object-center pt-2`;
+
 export default () => {
-  return <DateTimeDropdown />;
+  return (
+    <Container>
+      <Label>Period: </Label>
+      <DateTimeDropdown />
+    </Container>
+  );
 };
