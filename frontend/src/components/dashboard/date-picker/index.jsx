0c313287fc5451df7fb3200ee352561961cd0419
aleegs <aleej.gs@gmail.com>
2021-04-09 17:13:20 -0300

    Update styles

--- frontend/src/components/dashboard/date-picker/index.jsx
+++ frontend/src/components/dashboard/date-picker/index.jsx
@@ -1,8 +1,8 @@
 import tw from "twin.macro";
 import DatePicker from "./DatePicker";
 
-const Container = tw.div`mt-8 flex flex-row justify-center`;
-const Label = tw.text`text-gray-600 mr-3 top-0 object-center pt-2`;
+const Container = tw.div`mt-8 flex-col text-center flex sm:flex-row justify-center`;
+const Label = tw.text`text-gray-600 mr-3 object-center pt-1`;
 
 export default () => {
   return (
