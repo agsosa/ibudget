cff9ce170324c5fbfe1b16a47ef1557c2ff033d9
aleegs <aleej.gs@gmail.com>
2021-04-09 01:38:54 -0300

    Fix formatting, imports

--- frontend/src/treact/components/faqs/SingleCol.js
+++ frontend/src/treact/components/faqs/SingleCol.js
@@ -3,12 +3,18 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
-import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
-import { SectionDescription } from "components/misc/Typography.js";
-import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import {
+  SectionHeading,
+  Subheading as SubheadingBase,
+} from "treact/components/misc/Headings";
+import { SectionDescription } from "treact/components/misc/Typography.js";
+import {
+  Container,
+  ContentWithPaddingXl,
+} from "treact/components/misc/Layouts.js";
 import { ReactComponent as ChevronDownIcon } from "feather-icons/dist/icons/chevron-down.svg";
-import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-7.svg";
-import { ReactComponent as SvgDecoratorBlob2 } from "images/svg-decorator-blob-8.svg";
+import { ReactComponent as SvgDecoratorBlob1 } from "treact/images/svg-decorator-blob-7.svg";
+import { ReactComponent as SvgDecoratorBlob2 } from "treact/images/svg-decorator-blob-8.svg";
 
 const Subheading = tw(SubheadingBase)`mb-4 text-center`;
 const Heading = tw(SectionHeading)`w-full`;
@@ -27,7 +33,9 @@ const QuestionToggleIcon = motion.custom(styled.span`
     ${tw`w-6 h-6`}
   }
 `);
-const Answer = motion.custom(tw.dd`pointer-events-none text-sm sm:text-base leading-relaxed`);
+const Answer = motion.custom(
+  tw.dd`pointer-events-none text-sm sm:text-base leading-relaxed`
+);
 
 const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
   ${tw`pointer-events-none -z-20 absolute right-0 top-0 h-56 w-56 opacity-15 transform translate-x-2/3 -translate-y-12 text-teal-400`}
@@ -35,8 +43,6 @@ const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
 const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
   ${tw`pointer-events-none -z-20 absolute left-0 bottom-0 h-64 w-64 opacity-15 transform -translate-x-2/3 text-primary-500`}
 `;
-
-
 
 export default ({
   subheading = "FAQS",
@@ -46,28 +52,28 @@ export default ({
     {
       question: "Is lunch provided free of cost ?",
       answer:
-        "Yes, it is, if you have a membership with us. Otherwise it is charged as per the menu. Some limits do apply as to how much items can be included in your lunch. This limit is enough for any one person and merely exists to discourage abusal of the system."
+        "Yes, it is, if you have a membership with us. Otherwise it is charged as per the menu. Some limits do apply as to how much items can be included in your lunch. This limit is enough for any one person and merely exists to discourage abusal of the system.",
     },
     {
       question: "Do you have 2 Bedroom suites ?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
       question: "Are Wi-Fi costs included in the price ?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
       question: "Where can I reach you for support ?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    }
-  ]
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+  ],
 }) => {
   const [activeQuestionIndex, setActiveQuestionIndex] = useState(null);
 
-  const toggleQuestion = questionIndex => {
+  const toggleQuestion = (questionIndex) => {
     if (activeQuestionIndex === questionIndex) setActiveQuestionIndex(null);
     else setActiveQuestionIndex(questionIndex);
   };
@@ -95,11 +101,16 @@ export default ({
                   <QuestionToggleIcon
                     variants={{
                       collapsed: { rotate: 0 },
-                      open: { rotate: -180 }
+                      open: { rotate: -180 },
                     }}
                     initial="collapsed"
-                    animate={activeQuestionIndex === index ? "open" : "collapsed"}
-                    transition={{ duration: 0.02, ease: [0.04, 0.62, 0.23, 0.98] }}
+                    animate={
+                      activeQuestionIndex === index ? "open" : "collapsed"
+                    }
+                    transition={{
+                      duration: 0.02,
+                      ease: [0.04, 0.62, 0.23, 0.98],
+                    }}
                   >
                     <ChevronDownIcon />
                   </QuestionToggleIcon>
@@ -107,7 +118,7 @@ export default ({
                 <Answer
                   variants={{
                     open: { opacity: 1, height: "auto", marginTop: "16px" },
-                    collapsed: { opacity: 0, height: 0, marginTop: "0px" }
+                    collapsed: { opacity: 0, height: 0, marginTop: "0px" },
                   }}
                   initial="collapsed"
                   animate={activeQuestionIndex === index ? "open" : "collapsed"}
@@ -120,7 +131,7 @@ export default ({
           </FAQSContainer>
         </Column>
       </ContentWithPaddingXl>
-      <DecoratorBlob1/>
+      <DecoratorBlob1 />
       <DecoratorBlob2 />
     </Container>
   );
