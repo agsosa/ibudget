c574122800670bd168a4ee44bb0b74e3bd0ae3da
aleegs <aleej.gs@gmail.com>
2021-04-08 23:07:57 -0300

    Add guest-member links

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,9 +1,12 @@
+// TODO: Fix mobile nav menu X button not sticky
+
 import React from "react";
 import { motion } from "framer-motion";
 import tw from "twin.macro";
 import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
 import { Link } from "react-router-dom";
+import { Icon } from "react-bulma-components";
 
 import useAnimatedNavToggler from "treact/helpers/useAnimatedNavToggler";
 
@@ -15,8 +18,9 @@ import { APP_NAME } from "lib/Config";
 /* Start styled components */
 
 const Header = tw.header`
-  flex justify-between items-center
-  max-w-screen-xl mx-auto
+  flex items-center
+  justify-between
+  max-w-screen-2xl mx-auto
 `;
 
 const NavLinks = tw.div`inline-block`;
@@ -61,8 +65,13 @@ const MobileNavLinks = motion.custom(styled.div`
 `);
 
 const DesktopNavLinks = tw.nav`
-  hidden lg:flex flex-1 justify-between items-center
+  hidden lg:flex flex-1 items-center justify-between
 `;
+
+const AddTransactionBtn = tw(PrimaryLink)`p-3 ml-2`;
+const UserContainer = tw(NavLink)`flex inline`;
+const UserImage = tw.img`w-8 h-8 rounded-full inline`;
+const UserName = tw(NavLink)`ml-3 mr-1`;
 
 /* The below code is for generating dynamic break points for navbar.
  * Using this you can specify if you want to switch
@@ -96,18 +105,39 @@ const collapseBreakPointCssMap = {
 /* End styled components */
 
 // Links component to display
-const defaultLinks = [
-  <NavLinks key={1}>
+const defaultLinks = (
+  <>
     <NavLink to="/">Home</NavLink>
     <NavLink to="/how-it-works">How it Works</NavLink>
     <NavLink to="/contact-us">Contact Us</NavLink>
+  </>
+);
+
+const guestLinks = (
+  <>
     <SecondaryLink to="/dashboard">Log In</SecondaryLink>
 
     <PrimaryLink css={tw`rounded-full`} to="/register">
       Get Started
     </PrimaryLink>
-  </NavLinks>,
-];
+  </>
+);
+
+const memberLinks = (
+  <>
+    <NavLink to="/dashboard">Dashboard</NavLink>
+    <NavLink to="/transactions">Transactions</NavLink>
+    <NavLink to="/analytics">Analytics</NavLink>
+    <AddTransactionBtn css={tw`rounded-full`} to="/dashboard">
+      + Transaction
+    </AddTransactionBtn>
+    <UserContainer to="/account">
+      <UserImage src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3.25&w=512&h=512&q=80" />
+      <UserName to="/account">Enzo Vazquez</UserName>
+      <Icon icon="angle-down" />
+    </UserContainer>
+  </>
+);
 
 // Logo component to display
 const defaultLogoLink = (
@@ -121,11 +151,19 @@ export default () => {
   const { showNavLinks, animation, toggleNavbar } = useAnimatedNavToggler();
   const collapseBreakpointCss = collapseBreakPointCssMap.lg;
 
+  const isLogged = true;
+
+  const links = (
+    <NavLinks onClick={toggleNavbar}>
+      {isLogged ? memberLinks : [defaultLinks, guestLinks]}
+    </NavLinks>
+  );
+
   return (
     <Header className="header-light">
       <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
         {defaultLogoLink}
-        {defaultLinks}
+        {links}
       </DesktopNavLinks>
 
       <MobileNavLinksContainer
@@ -137,7 +175,7 @@ export default () => {
           animate={animation}
           css={collapseBreakpointCss.mobileNavLinks}
         >
-          {defaultLinks}
+          {links}
         </MobileNavLinks>
         <NavToggle
           onClick={toggleNavbar}
