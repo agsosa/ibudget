183e156ddbc9f839427a11c7d7ddb233b8c7c9c8
aleegs <aleej.gs@gmail.com>
2021-04-09 18:49:42 -0300

    Refactor period dropdown

--- frontend/src/components/dashboard/date-picker/DatePicker.jsx
+++ frontend/src/components/dashboard/date-picker/DatePicker.jsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { DateRange } from "react-date-range";
-import { addDays } from "date-fns"; // eslint-disable-line
-import tw from "twin.macro";
+import { subDays } from "date-fns"; // eslint-disable-line
+import tw, { styled } from "twin.macro";
 import { ReactComponent as CloseIcon } from "feather-icons/dist/icons/x.svg";
+import ReactSelect from "react-select";
 
 const Container = tw.div``;
 const DateComponent = tw(DateRange)`transform scale-x-90 sm:scale-x-100`;
-const Select = tw.select`rounded-lg w-56 border border-gray-300 p-1 align-middle disabled:bg-white hover:bg-primary-100 cursor-pointer`;
-const DropdownContent = tw.div`bg-white shadow-2xl flex flex-col z-20 absolute left-0 w-full sm:w-auto sm:left-auto`;
+const Select = tw(ReactSelect)`w-56`;
+const DropdownContent = styled.div(({ show }) => [
+  tw`bg-white shadow-2xl flex flex-col z-20 absolute left-0 w-full sm:w-auto sm:left-auto`,
+  !show && tw`hidden`,
+]);
 const Input = tw.input`m-1`;
 const InputGroup = tw.div`p-5 ml-3 sm:ml-0`;
 const InputContainer = tw.div`flex flex-row align-middle my-1`;
@@ -17,60 +21,85 @@ const CloseBtn = tw(
 )`absolute right-0 mt-3 mr-4 transform scale-90 cursor-pointer`;
 
 export default () => {
+  const containterRef = React.useRef(null);
+  const [isDropdownOpen, setDropdownOpen] = React.useState(false);
   const [state, setState] = React.useState([
     {
       startDate: new Date(),
-      endDate: addDays(new Date(), 7),
+      endDate: subDays(new Date(), 30),
       key: "selection",
     },
   ]);
 
-  function CustomSelect() {
+  function toggleDropdown() {
+    setDropdownOpen(!isDropdownOpen);
+  }
+
+  function handleClickOutside(event) {
+    if (
+      isDropdownOpen &&
+      containterRef.current &&
+      !containterRef.current.contains(event.target)
+    ) {
+      toggleDropdown();
+    }
+  }
+
+  React.useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isDropdownOpen]); // Add isDropdownOpen as dependency to re-register listener on state update
+
+  function Dropdown() {
     return (
-      <>
-        <Select disabled>
-          <option hidden selected>
-            Select placeholder
-          </option>
-        </Select>
-        <DropdownContent>
-          <CloseBtn />
-          <InputGroup>
-            <InputContainer>
-              <Input type="radio" />
-              <InputText>7 days</InputText>
-            </InputContainer>
-            <InputContainer>
-              <Input type="radio" />
-              <InputText>30 days</InputText>
-            </InputContainer>
-            <InputContainer>
-              <Input type="radio" />
-              <InputText>90 days</InputText>
-            </InputContainer>
-            <InputContainer>
-              <Input type="radio" />
-              <InputText>12 months</InputText>
-            </InputContainer>
-            <InputContainer>
-              <Input type="radio" />
-              <InputText>Custom range</InputText>
-            </InputContainer>
-          </InputGroup>
-          <DateComponent
-            editableDateInputs
-            onChange={(item) => setState([item.selection])}
-            moveRangeOnFirstSelection={false}
-            ranges={state}
-          />
-        </DropdownContent>
-      </>
+      <DropdownContent show={isDropdownOpen}>
+        <CloseBtn onClick={toggleDropdown} />
+        <InputGroup>
+          <InputContainer>
+            <Input type="radio" />
+            <InputText>7 days</InputText>
+          </InputContainer>
+          <InputContainer>
+            <Input type="radio" />
+            <InputText>30 days</InputText>
+          </InputContainer>
+          <InputContainer>
+            <Input type="radio" />
+            <InputText>90 days</InputText>
+          </InputContainer>
+          <InputContainer>
+            <Input type="radio" />
+            <InputText>12 months</InputText>
+          </InputContainer>
+          <InputContainer>
+            <Input type="radio" />
+            <InputText>Custom range</InputText>
+          </InputContainer>
+        </InputGroup>
+        <DateComponent
+          editableDateInputs
+          onChange={(item) => setState([item.selection])}
+          moveRangeOnFirstSelection={false}
+          ranges={state}
+        />
+      </DropdownContent>
     );
   }
 
   return (
-    <Container>
-      <CustomSelect />
+    <Container ref={containterRef}>
+      <Select
+        isSearchable={false}
+        onMenuOpen={toggleDropdown}
+        placeholder="xd"
+        components={{
+          Menu: () => null,
+        }}
+      />
+      <Dropdown />
     </Container>
   );
 };
