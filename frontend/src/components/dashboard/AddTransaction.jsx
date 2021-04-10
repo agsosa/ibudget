10b9682cca48278a63989d5665211698843beae3
aleegs <aleej.gs@gmail.com>
2021-04-10 17:26:01 -0300

    Refactor subcomponents to be a JSX variable instead of a function component

--- frontend/src/components/dashboard/AddTransaction.jsx
+++ frontend/src/components/dashboard/AddTransaction.jsx
@@ -23,9 +23,11 @@ import {
   InputFile,
 } from "react-bulma-components/lib/components/form";
 import Button from "react-bulma-components/lib/components/button";
-import Icon from "react-bulma-components/lib/components/icon";
+import Icon from "@mdi/react";
+import { mdiMinus, mdiPlus } from "@mdi/js";
 import { EnumTransactionType } from "lib/Enums";
 import SelectButtonGroup from "components/misc/SelectButtonGroup";
+import { TransactionModel } from "lib/Models";
 
 /* Start styled components */
 
@@ -61,19 +63,36 @@ const initialState = {
 function AddTransaction() {
   const [state, setState] = React.useState(initialState);
 
-  const { amount, notes, category, type, date, concept } = state;
-
   function onInputChange(evt) {
-    const value =
-      evt.target.type === "checkbox" ? evt.target.checked : evt.target.value;
-    setState({
-      [evt.target.name]: value,
-    });
+    let value;
+
+    // Parse/validate input
+    if (evt.target.type === "checkbox") {
+      value = evt.target.checked;
+    } else if (evt.target.name === "amount") {
+      const inputNumber = parseInt(evt.target.value, 10);
+      if (
+        inputNumber >= 0 &&
+        inputNumber <= TransactionModel.AMOUNT_MAX_NUMBER
+      ) {
+        value = inputNumber;
+      }
+    } else value = evt.target.value;
+
+    if (value) {
+      setState((oldState) => ({
+        ...oldState,
+        [evt.target.name]: value,
+      }));
+    }
   }
 
   function onTypeButtonClick(value) {
     if (Object.values(EnumTransactionType).includes(value)) {
-      setState({ ...state, type: value });
+      setState((oldState) => ({
+        ...oldState,
+        type: value,
+      }));
     } else {
       console.error(
         "onTypeButtonClick received invalid value. Value should be any of EnumTransactionType."
@@ -81,93 +100,109 @@ function AddTransaction() {
     }
   }
 
-  function CategoryDropdown() {
-    return (
-      <InputGroup>
-        <InputLabel>Categoría</InputLabel>
-      </InputGroup>
-    );
-  }
+  const CategoryDropdown = (
+    <InputGroup>
+      <InputLabel>Categoría</InputLabel>
+    </InputGroup>
+  );
 
-  function TypeSelect() {
-    return (
-      <InputGroup>
-        <InputLabel>Tipo de Transacción</InputLabel>
-        <SelectButtonGroup
-          selectedValue={type}
-          onValueSelect={onTypeButtonClick}
-        >
-          <SelectButtonGroup.Item
-            value={EnumTransactionType.OUT}
-            label="Expense"
-            borderLeft
-          />
-          <SelectButtonGroup.Item
-            value={EnumTransactionType.IN}
-            label="Income"
-            borderRight
-          />
-        </SelectButtonGroup>
-      </InputGroup>
-    );
-  }
-
-  function ConceptSelect() {
-    return (
-      <InputGroup>
-        <InputLabel>Concepto</InputLabel>
-      </InputGroup>
-    );
-  }
-
-  function NoteTextbox() {
-    return (
-      <InputGroup>
-        <InputLabel>Notas</InputLabel>
-
-        <Textarea
-          name="notes"
-          value={notes}
-          onChange={onInputChange}
-          placeholder="Additional notes..."
+  const TypeSelect = (
+    <InputGroup>
+      <InputLabel>Tipo de Transacción</InputLabel>
+      <SelectButtonGroup
+        selectedValue={state.type}
+        onValueSelect={onTypeButtonClick}
+      >
+        <SelectButtonGroup.Item
+          value={EnumTransactionType.OUT}
+          label="Expense"
+          borderLeft
         />
-      </InputGroup>
-    );
-  }
+        <SelectButtonGroup.Item
+          value={EnumTransactionType.IN}
+          label="Income"
+          borderRight
+        />
+      </SelectButtonGroup>
+    </InputGroup>
+  );
 
-  function DateSelect() {
-    return (
-      <InputGroup>
-        <InputLabel>Concepto</InputLabel>
-      </InputGroup>
-    );
-  }
+  const NoteTextbox = (
+    <InputGroup>
+      <InputLabel>Notas</InputLabel>
 
-  function AmountInput() {
-    return (
-      <InputGroup>
-        <InputLabel>Monto</InputLabel>
-      </InputGroup>
-    );
-  }
+      <Textarea
+        name="notes"
+        value={state.notes}
+        onChange={onInputChange}
+        placeholder="Additional notes..."
+      />
+    </InputGroup>
+  );
+
+  const ConceptSelect = (
+    <InputGroup>
+      <InputLabel>Concepto</InputLabel>
+    </InputGroup>
+  );
+
+  const DateSelect = (
+    <InputGroup>
+      <InputLabel>Concepto</InputLabel>
+    </InputGroup>
+  );
+
+  const AmountInput = (
+    <InputGroup>
+      <InputLabel>Monto</InputLabel>
+
+      <Control>
+        <Input
+          onChange={onInputChange}
+          name="amount"
+          type="number"
+          placeholder="0"
+          style={{
+            paddingLeft: 32,
+          }}
+          min={0}
+          max={TransactionModel.AMOUNT_MAX_NUMBER}
+          format={(value) => "asd"}
+          value={state.amount}
+        />
+        <Icon
+          path={state.type === EnumTransactionType.OUT ? mdiMinus : mdiPlus}
+          title="Money amount"
+          size={0.9}
+          style={{ position: "absolute", left: 5, top: "25%", opacity: 0.2 }}
+          horizontal
+          vertical
+          rotate={180}
+          color="black"
+        />
+      </Control>
+    </InputGroup>
+  );
 
   return (
     <>
       <InputContainer>
         <LeftContainer>
-          <TypeSelect />
-          <CategoryDropdown />
-          <NoteTextbox />
+          {TypeSelect}
+          {CategoryDropdown}
+          {NoteTextbox}
         </LeftContainer>
         <RightContainer>
-          <ConceptSelect />
-          <DateSelect />
-          <AmountInput />
+          {ConceptSelect}
+          {DateSelect}
+          {AmountInput}
         </RightContainer>
       </InputContainer>
       <ButtonsContainer>
         <PrimaryButton>Add Transaction</PrimaryButton>
         <SecondaryButton>Add and Create another</SecondaryButton>
+
+        {JSON.stringify(state)}
       </ButtonsContainer>
     </>
   );
