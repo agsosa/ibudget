a4fed2687b816f377fa0b9caab039eafc5257235
aleegs <aleej.gs@gmail.com>
2021-04-10 20:03:53 -0300

    Update TransactionModel notes max characters

--- frontend/src/lib/Models.js
+++ frontend/src/lib/Models.js
@@ -28,7 +28,7 @@ export const TransactionModel = {
     notes: null,
   },
   CONCEPT_MAX_CHARS: 25, // max TransactionModel's concept field length
-  NOTES_MAX_CHARS: 50, // max TransactionModel's notes field length
+  NOTES_MAX_CHARS: 100, // max TransactionModel's notes field length
   AMOUNT_MIN_NUMBER: 0.01,
   AMOUNT_MAX_NUMBER: 1000000000, // max TransactionModel's amount field number
   AMOUNT_MAX_DECIMALS: 2,
