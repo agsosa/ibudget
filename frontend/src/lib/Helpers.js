95922f4a0f6c1af6b5260ba9f1d5cf896d6f543f
aleegs <aleej.gs@gmail.com>
2021-04-11 02:49:18 -0300

    Move  getCategoryMaterialIcon and getCategoryBackgroundStyle to Helpers

--- frontend/src/lib/Helpers.js
+++ frontend/src/lib/Helpers.js
@@ -1,3 +1,17 @@
+import tw from "twin.macro";
+import {
+  mdiSilverware,
+  mdiCartOutline,
+  mdiHome,
+  mdiBus,
+  mdiCarHatchback,
+  mdiDevices,
+  mdiCurrencyUsd,
+  mdiCreditCardOutline,
+  mdiBriefcaseOutline,
+  mdiAccount,
+  mdiDotsVertical,
+} from "@mdi/js";
 import { CategoryEnum, PeriodEnum } from "./Enums";
 
 /* eslint-disable no-extend-native */
@@ -69,3 +83,81 @@ export function getCategoryLabel(categoryEnum) {
       return "";
   }
 }
+
+/**
+ * Get the associated tailwind background style for a category
+ * @param  {String} categoryEnum The category (must be a value of CategoryEnum)
+ * @return {TwFn}                Tailwind background style
+ */
+export const getCategoryBackgroundStyle = (categoryEnum) => {
+  switch (categoryEnum) {
+    case CategoryEnum.SHOPPING:
+      return tw`bg-orange-500`;
+    case CategoryEnum.COMMUNICATION_PC:
+      return tw`bg-orange-400`;
+    case CategoryEnum.FINANCIAL_EXPENSES:
+      return tw`bg-red-600`;
+    case CategoryEnum.FOOD_DRINKS:
+      return tw`bg-red-500`;
+    case CategoryEnum.HOUSING:
+      return tw`bg-indigo-600`;
+    case CategoryEnum.INCOME:
+      return tw`bg-yellow-400`;
+    case CategoryEnum.INVESTMENTS:
+      return tw`bg-green-500`;
+    case CategoryEnum.LIFE_ENTERTAINMENT:
+      return tw`bg-blue-500`;
+    case CategoryEnum.OTHERS:
+      return tw`bg-gray-500`;
+    case CategoryEnum.TRANSPORTATION:
+      return tw`bg-pink-500`;
+    case CategoryEnum.VEHICLE:
+      return tw`bg-purple-500`;
+    default:
+      return tw`bg-gray-700`;
+  }
+};
+
+// Get the associated material design icon for the category
+export const getCategoryMaterialIcon = (category) => {
+  let icon;
+  switch (category) {
+    case CategoryEnum.SHOPPING:
+      icon = mdiCartOutline;
+      break;
+    case CategoryEnum.FOOD_DRINKS:
+      icon = mdiSilverware;
+      break;
+    case CategoryEnum.HOUSING:
+      icon = mdiHome;
+      break;
+    case CategoryEnum.COMMUNICATION_PC:
+      icon = mdiDevices;
+      break;
+    case CategoryEnum.FINANCIAL_EXPENSES:
+      icon = mdiCreditCardOutline;
+      break;
+    case CategoryEnum.INCOME:
+      icon = mdiCurrencyUsd;
+      break;
+    case CategoryEnum.INVESTMENTS:
+      icon = mdiBriefcaseOutline;
+      break;
+    case CategoryEnum.LIFE_ENTERTAINMENT:
+      icon = mdiAccount;
+      break;
+    case CategoryEnum.OTHERS:
+      icon = mdiDotsVertical;
+      break;
+    case CategoryEnum.TRANSPORTATION:
+      icon = mdiBus;
+      break;
+    case CategoryEnum.VEHICLE:
+      icon = mdiCarHatchback;
+      break;
+    default:
+      icon = null;
+  }
+
+  return icon;
+};
