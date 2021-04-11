8120ae90c0306a893f73f14b0824770e50cc01c7
aleegs <aleej.gs@gmail.com>
2021-04-11 04:39:33 -0300

    Add dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
-// TODO: Add rate limit
+// TODO: Add node-rate-limiter-flexible
 // TODO: Stress test
-
+require("dotenv").config();
 const express = require("express");
 const compression = require("compression");
 const helmet = require("helmet");
@@ -11,7 +11,7 @@ port = process.env.PORT || 4000;
 
 const app = express();
 
-// Apply middlewares
+// Configure app
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,6 +19,7 @@ app.use(helmet());
 app.use(cors()); // TODO: Change before deploy!
 app.use(morgan("combined"));
 
+// Hadle root path, 404
 app.get("/", (req, res) => res.send("OK"));
 
 app.use(function (req, res, next) {
