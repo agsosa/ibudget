67764553a8c73bf25ea0137bb2be89e8a552e2d6
aleegs <aleej.gs@gmail.com>
2021-04-11 19:35:13 -0300

    Add Rematch plugins

--- frontend/src/lib/Store.js
+++ frontend/src/lib/Store.js
@@ -1,6 +1,25 @@
 import { init } from "@rematch/core";
+import loadingPlugin from "@rematch/loading";
+import updatedPlugin from "@rematch/updated";
+import selectPlugin from "@rematch/select";
+import persistPlugin from "@rematch/persist";
+import storage from "redux-persist/lib/storage";
 import * as models from "./Models";
 
-const store = init({ name: "MainStore", models });
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const store = init({
+  name: "MainStore",
+  models,
+  plugins: [
+    persistPlugin(persistConfig),
+    updatedPlugin(),
+    loadingPlugin(),
+    selectPlugin(),
+  ],
+});
 
 export default store;
