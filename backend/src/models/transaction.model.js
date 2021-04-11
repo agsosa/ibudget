7cc7985e47d24ec46b6b8d63193101c8656bcd02
aleegs <aleej.gs@gmail.com>
2021-04-11 18:41:59 -0300

    Add findAll, update delete

--- backend/src/models/transaction.model.js
+++ backend/src/models/transaction.model.js
@@ -15,7 +15,9 @@ const TransactionModel = {
   }),
 };
 
-TransactionModel.create = (transaction_info, user_id) => {
+TransactionModel.create = (user_id, transaction_info) => {
+  // TODO: Validate user_id
+
   const isValidInfo = TransactionModel.infoSchema.validate(transaction_info);
 
   console.log(JSON.stringify(isValidInfo));
@@ -26,10 +28,35 @@ TransactionModel.create = (transaction_info, user_id) => {
 };
 
 TransactionModel.delete = (id) => {
-  const query = `DELETE FROM ${TABLE_NAME} WHERE \`id\` =  ?`;
-  const params = [id];
+  // TODO: Check user_id permission!!!
+  return new Promise((resolve, reject) => {
+    const query = `DELETE FROM ${TABLE_NAME} WHERE \`id\` =  ?`;
+    const params = [id];
 
-  return database.execute(query, params);
+    database
+      .execute(query, params)
+      .then(([rows]) => {
+        if (rows.affectedRows === 0) reject("id not found");
+        else resolve();
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+TransactionModel.findAll = async (user_id) => {
+  return new Promise((resolve, reject) => {
+    // TODO: Validate user_id?
+    if (user_id == null || typeof user_id !== "number")
+      reject("Specified user_id is not valid");
+
+    const query = `SELECT id, amount, category_id, type_id, date, concept, notes FROM ${TABLE_NAME} WHERE \`user_id\` =  ?`;
+    const params = [user_id];
+
+    database
+      .execute(query, params)
+      .then(([rows]) => resolve(rows))
+      .catch((err) => reject(err));
+  });
 };
 
 TransactionModel.update = () => {};
