0c95f08012c94c069787be21e84cd43693ae8f53
aleegs <aleej.gs@gmail.com>
2021-04-12 16:01:17 -0300

    Handle stack trace errors depending on NODE_ENV

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,31 +2,56 @@
 // TODO: Stress test
 require("dotenv").config();
 require("module-alias/register");
+const database = require("@lib/database");
 const express = require("express");
 const compression = require("compression");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
-const database = require("@data/database");
+const routes = require("@routes");
+const utils = require("@lib/utils");
 
-port = process.env.PORT || 4000;
+const env = process.env.NODE_ENV || "development";
+port = process.env.PORT || 4002;
 const app = express();
 
-// Add middlewares
+// Add global middlewares
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
 app.use(cors()); // TODO: Change before deploy!
-app.use(morgan("combined"));
+// app.use(morgan("combined")); // TODO: CHECK
 
-// Start app modules
+// Add routes middlewares
+app.use("/api/transactions", routes.transactions);
 
-// Hadle root path, 404
+// Handle root path
 app.get("/", (req, res) => res.send("OK"));
 
+// Show stack trace errors on development environment
+if (env === "development") {
+  app.use(function (err, req, res, next) {
+    console.error(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      error: true,
+      message: err.message,
+      stack: err.stack,
+    });
+  });
+}
+
+// Hide stack trace errors on production environment
+app.use(function (err, req, res, next) {
+  utils.sendFailedResponse(res, err.message, err.status || 500);
+});
+
+// Handle 404
 app.use(function (req, res, next) {
-  res.status(404).send("ERROR");
+  utils.sendFailedResponse(res, "endpoint not found", 404);
 });
 
 // Start server
@@ -34,6 +59,7 @@ app.listen(port);
 console.log("Running server on port", port);
 
 // Graceful shutdown
+// TODO: TEST!!
 process.on("SIGTERM", () => {
   debug("SIGTERM signal received: shutdown app");
   database.end(() => {
