23c36c0801f13461923dfc1582abf4506afae97a
aleegs <aleej.gs@gmail.com>
2021-04-12 19:27:58 -0300

    Implement createTransaction api request

--- frontend/src/lib/API.js
+++ frontend/src/lib/API.js
@@ -1,5 +1,5 @@
 // TODO: Add automatic retry?
-
+/* eslint-disable */
 import axios from "axios";
 
 const DEV_BASE_URL = "http://localhost:4002/api";
@@ -10,15 +10,78 @@ const API_BASE_URL =
     ? DEV_BASE_URL
     : PROD_BASE_URL;
 
-// TRANSACTIONS ENDPOINT
+function handleAxiosError(error) {
+  console.log("handleAxiosError ", error);
+}
+
+// TODO: Merge all the requests in one function and create an apiModule object
+
+// GET: transactions
 let getTransactionsPromise = null; // Store the current promise to prevent concurrent requests
 export function getTransactions() {
   const endpoint = "/transactions";
 
   // TODO: TEST
   if (!getTransactionsPromise) {
-    getTransactionsPromise = axios.get(API_BASE_URL + endpoint);
+    getTransactionsPromise = new Promise((resolve, reject) => {
+      axios
+        .get(API_BASE_URL + endpoint)
+        .then(({ data }) => {
+          if (data.error) {
+            throw new Error(data.error);
+          } else resolve(data);
+        })
+        .catch((error) => {
+          let errorData;
+
+          if (error && error.response && error.response.data)
+            errorData = error.response.data;
+          else
+            errorData = {
+              error: true,
+              message: error.message || "No error message",
+            };
+
+          handleAxiosError(errorData);
+          reject(errorData);
+        });
+    });
   }
 
   return getTransactionsPromise;
+}
+
+// POST: transaction
+let createTransactionsPromise = null; // Store the current promise to prevent concurrent requests
+export function createTransaction(payload) {
+  const endpoint = "/transactions";
+
+  // TODO: TEST
+  if (!createTransactionsPromise) {
+    createTransactionsPromise = new Promise((resolve, reject) => {
+      axios
+        .post(API_BASE_URL + endpoint, payload)
+        .then(({ data }) => {
+          if (data.error) {
+            throw new Error(data.error);
+          } else resolve(data);
+        })
+        .catch((error) => {
+          let errorData;
+
+          if (error && error.response && error.response.data)
+            errorData = error.response.data;
+          else
+            errorData = {
+              error: true,
+              message: error.message || "No error message",
+            };
+
+          handleAxiosError(errorData);
+          reject(errorData);
+        });
+    });
+  }
+
+  return createTransactionsPromise;
 }
