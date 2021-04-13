931adade3b63295fafe3ff7ed8c6da16d8abc88a
aleegs <aleej.gs@gmail.com>
2021-04-13 13:39:54 -0300

    Update comments

--- backend/src/models/user.model..js
+++ backend/src/models/user.model..js
@@ -1,3 +1,7 @@
+// Docs: https://documenter.getpostman.com/view/13863838/TzCV45Ku
+
+// TODO: Add a "lastUpdate" endpoint to record the last time the client sent a update so we can use this to implement cache on client
+
 class UserModel {}
 
 module.exports = new UserModel();
--- backend/src/routes/transactions.routes.js
+++ backend/src/routes/transactions.routes.js
@@ -1,18 +1,18 @@
+// Docs: https://documenter.getpostman.com/view/13863838/TzCV45Ku
 const express = require("express");
 const router = express.Router();
 const controller = require("@controllers/transaction.controller");
 
 // Retrieve all transactions
-router.get("/", controller.findAll);
+router.get("/", controller.findAll); // TODO: Remove notes from this endpoint and create another endpoint to grab the notes for a single transaction id?
 
 // Create a new transaction
 router.post("/", controller.create);
 
-// Retrieve a single employee with id
-//router.get("/:id", controller.delete);
+// Update a transaction's info (full replace) with id
+router.put("/:id", controller.fullUpdate);
 
-// Update a employee with id
-//router.put("/:id", controller.delete);
+// TODO: Implement partialUpdate (patch)
 
 // Delete a transaction with id
 router.delete("/:id", controller.delete);
