f3ce688df32dfbd255f63e15db9a3ccbf68c5a13
aleegs <aleej.gs@gmail.com>
2021-04-13 16:26:14 -0300

    Create ContentWithPadding

---
+++ frontend/src/components/layout/ContentWithPadding.jsx
@@ -0,0 +1,9 @@
+import tw from "twin.macro";
+
+const ContentWithPadding = tw.div`
+mx-auto 
+px-0 py-10 sm:px-6 
+md:px-8 lg:px-12 xl:px-24 
+sm:py-10 flex flex-col max-w-full`;
+
+export default ContentWithPadding;
