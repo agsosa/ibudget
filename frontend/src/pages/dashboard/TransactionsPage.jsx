d32c6271e588c2e8ed24ec9b0a9811470cb961f6
aleegs <aleej.gs@gmail.com>
2021-04-14 00:50:43 -0300

    Add sort by id, update styles

--- frontend/src/pages/dashboard/TransactionsPage.jsx
+++ frontend/src/pages/dashboard/TransactionsPage.jsx
@@ -1,17 +1,11 @@
-/* eslint-disable */
 import * as React from "react";
-// import Articles from "components/dashboard/Articles";
-import CardList from "components/misc/CardList";
 import tw from "twin.macro";
 import DateRangeSelector from "components/dashboard/smart-components/DateRangeSelector";
-import LatestTransactions from "components/dashboard/smart-components/LatestTransactions";
 import { useSelector } from "react-redux";
 import store from "lib/Store";
-import CloudLoadingIndicator from "components/misc/CloudLoadingIndicator";
 import ContentWithPadding from "components/layout/ContentWithPadding";
 import withFetchTransactions from "components/dashboard/smart-components/withFetchTransactions";
 import NoDataIndicator from "components/misc/NoDataIndicator";
-import RadioGroup from "components/misc/input/RadioGroup";
 import { CategoryEnum, TransactionTypeEnum } from "ibudget-shared";
 import CheckboxGroup from "react-checkbox-group";
 import {
@@ -25,33 +19,37 @@ import CategoryIcon from "components/dashboard/CategoryIcon";
 import Accordion from "components/misc/Accordion";
 import TransactionList from "components/dashboard/TransactionList";
 import Skeleton from "react-loading-skeleton";
-import Icon from "@mdi/react";
-import { mdiInformationOutline } from "@mdi/js";
 import { SortModeEnum } from "lib/Enums";
+import { PropTypes } from "prop-types";
 
 /* Start styled components */
 
 const HeaderContainer = tw.div`w-full flex flex-col items-center mb-8 `;
 const DateRangeContainer = tw.div`flex-col text-center flex sm:flex-row justify-center align-middle`;
-const DateRangeLabel = tw.text`text-gray-600 mr-3 mt-2`;
+const DateRangeLabel = tw.text`text-gray-700 mr-3 mt-2`;
 const Container = tw.div`
-self-center gap-3 flex flex-col w-screen mx-5
-md:grid md:grid-cols-9 md:gap-6 
-md:max-w-screen-xl md:w-full`;
+self-center gap-3 flex flex-col 
+w-screen
+md:grid md:gap-6 
+sm:max-w-screen-xl sm:w-full
+md:grid-cols-9`;
 const LeftColumn = tw.div`
-col-span-3 flex flex-col 
+sm:col-span-4 md:col-span-3
+flex flex-col 
 shadow-sm rounded-xl 
-p-8 px-12 md:p-4 lg:p-8 
+p-4 ml-1 lg:p-8 
 bg-white`;
-const RightColumn = tw.div`col-span-6 shadow-sm rounded-xl p-10 bg-white`;
-const Title = tw.text`text-xl p-3 sm:p-0 md:text-2xl font-semibold`;
-const Hint = tw.text` text-sm p-1 mt-1 flex flex-row gap-1 items-center`;
+const RightColumn = tw.div`col-span-6 shadow-sm rounded-xl p-6 bg-white`;
+const Title = tw.text`self-center md:self-auto text-xl p-2 sm:p-0 md:text-lg lg:text-2xl font-semibold`;
+const Hint = tw.text`self-center md:self-auto text-sm p-1 flex flex-row gap-1 items-center text-gray-800`;
 const CategoryCheckboxContainer = tw.div`flex flex-row align-middle items-center gap-1`;
 const CategoryCheckboxLabel = tw.text` text-base`;
 const CustomSkeleton = tw(Skeleton)`mt-5 min-w-full`;
+const AccordionContainer = tw.div`ml-5 sm:self-center w-full md:self-start md:ml-0 xl:ml-5`;
 
 /* End style components */
 
+const LIMIT_PER_PAGE = 10;
 const ALL_FILTER = -1; // Value used to represent the "select all"
 const categoryFilterOptions = [ALL_FILTER, ...Object.values(CategoryEnum)];
 const typeFilterOptions = Object.values(TransactionTypeEnum);
@@ -98,23 +96,26 @@ function TransactionsPage({ loading }) {
     });
 
     // Sort
+    // eslint-disable-next-line
     switch (sortModeArray[0]) {
       case SortModeEnum.DATE_DESCENDING:
-        filteredAndSorted.sort((a, b) => b.date - a.date);
+        filteredAndSorted.sort((a, b) => b.date - a.date || b.id - a.id);
         break;
       case SortModeEnum.DATE_ASCENDING:
-        filteredAndSorted.sort((a, b) => a.date - b.date);
+        filteredAndSorted.sort((a, b) => a.date - b.date || a.id - b.id);
         break;
       case SortModeEnum.AMOUNT_DESCENDING:
         filteredAndSorted.sort(
           (a, b) =>
-            getTransactionAmountWithSign(b) - getTransactionAmountWithSign(a)
+            getTransactionAmountWithSign(b) - getTransactionAmountWithSign(a) ||
+            b.id - a.id
         );
         break;
       case SortModeEnum.AMOUNT_ASCENDING:
         filteredAndSorted.sort(
           (a, b) =>
-            getTransactionAmountWithSign(a) - getTransactionAmountWithSign(b)
+            getTransactionAmountWithSign(a) - getTransactionAmountWithSign(b) ||
+            a.id - b.id
         );
         break;
     }
@@ -127,21 +128,23 @@ function TransactionsPage({ loading }) {
     valuesArray /* Array with values of categoryFilterOptions */
   ) {
     if (valuesArray.length === 0) {
+      // No category check
       setFilterCategoryArray(valuesArray);
+      return;
+    }
+
+    if (
+      valuesArray.includes(ALL_FILTER) &&
+      !categoryFilterArray.includes(ALL_FILTER)
+    ) {
+      // Uncheck every category except ALL_CATEGORIES if the user checked the all categories option
+      setFilterCategoryArray([ALL_FILTER]);
+    } else if (categoryFilterArray.includes(ALL_FILTER)) {
+      // Uncheck ALL_CATEGORIES if it's already checked and the user checked a category
+      setFilterCategoryArray(valuesArray.filter((q) => q !== ALL_FILTER));
     } else {
-      if (
-        valuesArray.includes(ALL_FILTER) &&
-        !categoryFilterArray.includes(ALL_FILTER)
-      ) {
-        // Uncheck every category except ALL_CATEGORIES if the user checked the all categories option
-        setFilterCategoryArray([ALL_FILTER]);
-      } else if (categoryFilterArray.includes(ALL_FILTER)) {
-        // Uncheck ALL_CATEGORIES if it's already checked and the user checked a category
-        setFilterCategoryArray(valuesArray.filter((q) => q !== ALL_FILTER));
-      } else {
-        // Remaining cases with ALL_CATEGORIES unchecked
-        setFilterCategoryArray(valuesArray);
-      }
+      // Remaining cases with ALL_CATEGORIES unchecked
+      setFilterCategoryArray(valuesArray);
     }
   }
 
@@ -256,7 +259,11 @@ function TransactionsPage({ loading }) {
     if (localTransactions && localTransactions.length > 0)
       return (
         <>
-          <TransactionList loading limit={20} data={localTransactions} />
+          <TransactionList
+            loading
+            limit={LIMIT_PER_PAGE}
+            data={localTransactions}
+          />
         </>
       );
 
@@ -289,27 +296,20 @@ function TransactionsPage({ loading }) {
               !loading &&
               ` (${bigNumberFormatter(transactions.length)})`}
           </Title>
-          <Hint>
-            <Icon
-              path={mdiInformationOutline}
-              title="Hint"
-              size={0.75}
-              style={{ opacity: 0.5 }}
-              color="black"
+          <Hint>Click a transaction to edit or delete it</Hint>
+          <AccordionContainer>
+            <Accordion
+              isMulti
+              items={[
+                { title: "Orden", contentComponent: SortModeComponent },
+                { title: "Tipo", contentComponent: TypeFilterComponent },
+                {
+                  title: "Categoría",
+                  contentComponent: CategoryFilterComponent,
+                },
+              ]}
             />
-            Click a transaction to edit or delete it
-          </Hint>
-          <Accordion
-            isMulti
-            items={[
-              { title: "Orden", contentComponent: SortModeComponent },
-              { title: "Tipo", contentComponent: TypeFilterComponent },
-              {
-                title: "Categoría",
-                contentComponent: CategoryFilterComponent,
-              },
-            ]}
-          />
+          </AccordionContainer>
         </LeftColumn>
         <RightColumn>
           <RightColumnContent />
@@ -318,5 +318,9 @@ function TransactionsPage({ loading }) {
     </ContentWithPadding>
   );
 }
+
+TransactionsPage.propTypes = {
+  loading: PropTypes.bool.isRequired, // Injected by withFetchTransactions
+};
 
 export default withFetchTransactions(TransactionsPage);
