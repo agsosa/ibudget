9103f5ff85e4b7ae1d3f0e2a8230cd2e4a3f088b
aleegs <aleej.gs@gmail.com>
2021-04-14 16:29:26 -0300

    Add user controller

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,63 @@
+const bcrypt = require("bcrypt-nodejs");
+const UserModel = require("@models/user.model");
+const utils = require("@lib/utils");
+
+// Passport-local persistent session serialize function
+exports.serializeUser = function (user, done) {
+  done(null, user.id);
+};
+
+// Passport-local persistent session deserialize function
+exports.deserializeUser = function (id, done) {
+  UserModel.findById(id)
+    .then((result) => {
+      done(null, result);
+    })
+    .catch((err) => {
+      done(err, null);
+    });
+};
+
+// TODO: Implement bcrypt
+exports.login = function (req, username, password, done) {
+  UserModel.findByUsername(username)
+    .then((result) => {
+      if (!result) {
+        // No rows from database for this username
+        done(new Error("Username not found"), null);
+      } else {
+        if (password !== result.password)
+          // Wrong password
+          done(new Error("Wrong password provided"), null);
+        else {
+          // Auth success
+          done(null, result);
+        }
+      }
+    })
+    .catch((err) => done(err));
+};
+
+// onLoginSuccess: Called if the authentication was sucessful
+// req.user will contain the authenticated user
+exports.onLoginSuccess = (req, res) => {
+  /* if (req.body.remember) {
+    req.session.cookie.maxAge = 24 * 60 * 60 * 1000;
+  } else {
+    req.session.cookie.expires = false;
+  } */
+
+  utils.sendSuccessResponse(res, "Authenticated", { name: req.user.name });
+};
+
+exports.register = function (req, username, password, done) {
+  console.log("local-register callback");
+};
+
+/* 
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+
+    res.redirect("/");
+  }
+*/
