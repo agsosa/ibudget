92d28c6a8244f9af1dadbf9dcd2275ddbc576abc
aleegs <aleej.gs@gmail.com>
2021-04-14 16:30:15 -0300

    Add PassportConfig

---
+++ backend/src/lib/passport.config.js
@@ -0,0 +1,43 @@
+const LocalStrategy = require("passport-local").Strategy;
+const UserController = require("@controllers/user.controller");
+
+// Configure passport
+class PassportConfig {
+  constructor() {}
+
+  config(passport) {
+    // Setup passport serialize/deserialize
+    passport.serializeUser(UserController.serializeUser);
+    passport.deserializeUser(UserController.deserializeUser);
+
+    // Setup passport-local login strategy
+    passport.use(
+      "local-login",
+      new LocalStrategy(
+        {
+          usernameField: "username",
+          passwordField: "password",
+          passReqToCallback: true,
+        },
+        UserController.login // Pass callback to user controller
+      )
+    );
+
+    // Setup passport-local register strategy
+    passport.use(
+      "local-register",
+      new LocalStrategy(
+        {
+          usernameField: "username",
+          passwordField: "password",
+          passReqToCallback: true,
+        },
+        UserController.register
+      )
+    );
+
+    console.log("[Passport] Initialized");
+  }
+}
+
+module.exports = new PassportConfig();
