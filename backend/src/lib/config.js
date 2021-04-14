3b82cbca9dd85ff66e213a2e979d56259a1b477a
aleegs <aleej.gs@gmail.com>
2021-04-14 04:21:03 -0300

    Update config variables

--- backend/src/lib/config.js
+++ backend/src/lib/config.js
@@ -2,10 +2,19 @@ exports.env = process.env.NODE_ENV || "development";
 
 exports.serverPort = process.env.PORT || 4002;
 
-exports.dbConfig = {
+exports.dbPoolConfig = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+};
+
+exports.sessionConfig = {
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
 };
