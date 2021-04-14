c6c8ae69c0745f894bb2265373de4a18f6b94609
aleegs <aleej.gs@gmail.com>
2021-04-14 04:51:13 -0300

    Create user/auth routes/controller files

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,6 @@
+// Docs: https://documenter.getpostman.com/view/13863838/TzCV45Ku
+const express = require("express");
+const router = express.Router();
+const controller = require("@controllers/user.controller");
+
+module.exports = router;
