98b12eb4588c65a6203d23182dad75b453347e41
aleegs <aleej.gs@gmail.com>
2021-04-15 02:52:12 -0300

    Add joi schema, validation, create method

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,13 +2,46 @@ const database = require("@lib/database");
 const shared = require("ibudget-shared");
 const Joi = require("joi");
 const dateFns = require("date-fns");
+const shared = require("ibudget-shared");
+var bcrypt = require("bcrypt-nodejs");
 
+const saltRounds = 10;
 const TABLE_NAME = "users";
 // const SELECT_COLUMNS = "id, amount, category_id, type_id, date, concept, notes";
 
 const UserModel = {
-  infoSchema: Joi.object({}).unknown(),
+  userSchema: Joi.object({
+    password: Joi.string()
+      .min(shared.Limits.PASSWORD_MIN_CHARS)
+      .max(shared.Limits.PASSWORD_MAX_CHARS)
+      .required(),
+    username: Joi.string().email().required(),
+    name: Joi.string()
+      .min(shared.Limits.USER_NICK_MIN_CHARS)
+      .max(shared.Limits.USER_NICK_MAX_CHARS)
+      .required(),
+  }).unknown(),
 };
+
+function validateUserInfo(userInfo, reject) {
+  // Joi validation
+  const isValidInfo = userInfo && UserModel.userSchema.validate(userInfo);
+
+  // Joi: If the info is not valid then isValidInfo.error will exist and details.message will exist
+  if (!isValidInfo || isValidInfo.error) {
+    if (reject) {
+      reject(
+        `The specified user information is not valid. ${
+          isValidInfo ? isValidInfo.error : ""
+        }`
+      );
+    }
+
+    return false;
+  }
+
+  return true;
+}
 
 function validateUserId(id, reject) {
   if (id == null || Number.isNaN(id) || typeof id !== "number") {
@@ -47,6 +80,32 @@ UserModel.findByUsername = (username) => {
         resolve(rows);
       })
       .catch((err) => reject(err));
+  });
+};
+
+/* 
+  Create a new user
+
+  NOTE: THE PASSWORD WILL BE ENCRYPTED BEFORE INSERTING IT INTO THE DATABASE
+*/
+UserModel.create = (userInfo) => {
+  return new Promise((resolve, reject) => {
+    if (!validateUserInfo(userInfo, reject)) return;
+    const { name, username, password } = userInfo;
+
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+      if (!err) {
+        const query = `INSERT INTO ${TABLE_NAME} (name, username, password) VALUES (?, ?, ?)`;
+        const params = [name, username, hash];
+
+        database
+          .execute(query, params)
+          .then(([[rows]]) => {
+            resolve({ ...userInfo, id: rows.id });
+          })
+          .catch((err) => reject(err));
+      } else reject(err);
+    });
   });
 };
 
